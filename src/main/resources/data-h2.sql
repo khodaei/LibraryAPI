c0b33492a5f2d471cb56120f54748f3490ee9953
Amir Khodaei <amirk88@gmail.com>
2017-10-09 14:27:14 -0700

    changes sample data

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,4 +1,4 @@
 INSERT INTO user (username) VALUES ('amir');
-INSERT INTO book (id, title, author) VALUES (1,'Power of Habits', 'Amir Khodaei');
-INSERT INTO book (id, title, author) VALUES (2,'title2', 'author2');
+INSERT INTO book (id, title, author) VALUES (1,'The Power of Habit', 'Charles Duhigg');
+INSERT INTO book (id, title, author) VALUES (2,'The Pragmatic Programmer', 'Andrew Hunt');
 INSERT INTO user_book (user_username, book_id, status) VALUES ('amir', 1, 1);
